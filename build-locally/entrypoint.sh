6ce3467e32779b371951d49cb27d5503b3ffb249
Zoran Nikolic <zoran.nik@gmail.com>
2025-10-07 06:30:37 +0200

    looks like I will have to perform mega cleaning when building for different shields

--- build-locally/entrypoint.sh
+++ build-locally/entrypoint.sh
@@ -32,10 +32,10 @@ git config --global --add safe.directory "$BUILD_DIR/zephyr"
 export "base_dir=$BASE_DIR"
 export "build_dir=$BUILD_DIR"
 export "zephyr_version=${ZEPHYR_VERSION}"
-export "extra_west_args=-S studio-rpc-usb-uart"
-export "extra_cmake_args=${shield:+-DSHIELD=\"$shield\"}${zmk_load_arg}"
-export "display_name=${shield:+$shield_left - }${board}"
-export "artifact_name=${artifact_name:-${shield:+$shield-}${board}-zmk}"
+# export "extra_west_args=-S studio-rpc-usb-uart"
+# export "extra_cmake_args=${shield:+-DSHIELD=\"$shield\"}${zmk_load_arg}"
+# export "display_name=${shield:+$shield_left - }${board}"
+# export "artifact_name=${artifact_name:-${shield:+$shield-}${board}-zmk}"
 
 west init -l "$BASE_DIR/config"
 
@@ -48,7 +48,10 @@ west zephyr-export
 west build -s zmk/app -d "$BUILD_DIR" -b "nice_nano_v2" -S "studio-rpc-usb-uart" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="splitkb_aurora_lily58_left" -DZMK_EXTRA_MODULES="$BUILD_DIR" -DCONFIG_ZMK_STUDIO=y || exit 1
 cp "$BUILD_DIR/zephyr/zmk.uf2" "/build/splitkb_aurora_lily58_left-nice_nano_v2-zmk.uf2"
 
-export "display_name=${shield:+$shield_right - }${board}"
+# export "extra_west_args="
+# export "extra_cmake_args="
+# export "artifact_name=splitkb_aurora_lily58_right-nice_nano_v2-zmk"
+# export "display_name=${shield:+$shield_right - }${board}"
 west build -s zmk/app -d "$BUILD_DIR" -b "nice_nano_v2" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="splitkb_aurora_lily58_right" -DZMK_EXTRA_MODULES="$BUILD_DIR" -DCONFIG_ZMK_STUDIO=y || exit 1
 cp "$BUILD_DIR/zephyr/zmk.uf2" "/build/splitkb_aurora_lily58_right-nice_nano_v2-zmk.uf2"
 
