befe9efb68e7f0f938d2d77985ac946da8bad6b0
Zoran Nikolic <znikolic@allshare.net>
2025-10-07 11:46:20 +0200

    feexed all z problems with z build

--- build-locally/entrypoint.sh
+++ build-locally/entrypoint.sh
@@ -5,57 +5,49 @@ BUILD_DIR="$(mktemp -d)"
 BASE_DIR="/tmp/zmk-config"
 ZMK_CONFIG_DIR="$BASE_DIR/config"
 
-cat <<-EOF
-Base dir: $BASE_DIR
-Build dir: $BUILD_DIR
-ZMK CFG: $ZMK_CONFIG_DIR
-cp -R "$LILY58/config" "$ZMK_CONFIG_DIR"
-cp -R "$LILY58/*" "$BUILD_DIR"
-EOF
+export "base_dir=$BASE_DIR"
+export "zephyr_version=${ZEPHYR_VERSION}"
 
 board="nice_nano_v2"
-shield_left="splitkb_aurora_lily58_left"
-shield_right="splitkb_aurora_lily58_right"
-shield_reset="settings_reset"
-zmk_load_arg=" -DZMK_EXTRA_MODULES=$BUILD_DIR"
 
 mkdir -p "$BASE_DIR"
-cp -Rf $LILY58/config "$ZMK_CONFIG_DIR"
-cp -Rf $LILY58/* "$BUILD_DIR"
+cp -Rf $LILY58/config "$BASE_DIR"
+
+# we are not copyin recursively because now we have this temp directory that is enormeus
+# cp -R $LILY58/boards "$BASE_DIR"
+# cp -R $LILY58/config "$BASE_DIR"
+# cp -R $LILY58/zephyr "$BASE_DIR"
+# we probably dont need to copy since we can reference this from EXTRA_MODULES
 
 git config --global --add safe.directory "$BASE_DIR"
-git config --global --add safe.directory "$BASE_DIR/zephyr"
 git config --global --add safe.directory "$BUILD_DIR"
-git config --global --add safe.directory "$BUILD_DIR/zephyr"
 
-# export "CMAKE_PREFIX_PATH=$BUILD_DIR/zephyr:$CMAKE_PREFIX_PATH"
-export "base_dir=$BASE_DIR"
-export "build_dir=$BUILD_DIR"
-export "zephyr_version=${ZEPHYR_VERSION}"
-# export "extra_west_args=-S studio-rpc-usb-uart"
-# export "extra_cmake_args=${shield:+-DSHIELD=\"$shield\"}${zmk_load_arg}"
-# export "display_name=${shield:+$shield_left - }${board}"
-# export "artifact_name=${artifact_name:-${shield:+$shield-}${board}-zmk}"
-
-west init -l "$BASE_DIR/config"
+west init -l "$ZMK_CONFIG_DIR"
 
 cd "$BASE_DIR"
-echo "Update west ..."
+echo "=====> Update west ..."
 west update --fetch-opt=--filter=tree:0
-echo "west zephyr export ..."
+echo "=====> west zephyr export ..."
 west zephyr-export
 
-west build -p -s zmk/app -d "$BUILD_DIR" -b "nice_nano_v2" -S "studio-rpc-usb-uart" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="splitkb_aurora_lily58_left" -DZMK_EXTRA_MODULES="$BUILD_DIR" -DCONFIG_ZMK_STUDIO=y || exit 1
+artifact_name=splitkb_aurora_lily58_left-nice_nano_v2-zmk
+export artifact_name=$artifact_name
+export display_name="splitkb_aurora_lily58_left nice_nano_v2"
+left=$(mktemp -d)
+west build -p always -s zmk/app -d "$left" -b "$board" -S "studio-rpc-usb-uart" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="splitkb_aurora_lily58_left" -DZMK_EXTRA_MODULES="$LILY58" -DCONFIG_ZMK_STUDIO=y || exit 1
 cp "$BUILD_DIR/zephyr/zmk.uf2" "/build/splitkb_aurora_lily58_left-nice_nano_v2-zmk.uf2"
+cp $left/zephyr/zmk.uf2 "/build/$artifact_name.uf2"
 
-# export "extra_west_args="
-# export "extra_cmake_args="
-# export "artifact_name=splitkb_aurora_lily58_right-nice_nano_v2-zmk"
-# export "display_name=${shield:+$shield_right - }${board}"
-west build -p -s zmk/app -d "$BUILD_DIR" -b "nice_nano_v2" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="splitkb_aurora_lily58_right" -DZMK_EXTRA_MODULES="$BUILD_DIR" -DCONFIG_ZMK_STUDIO=y || exit 1
-cp "$BUILD_DIR/zephyr/zmk.uf2" "/build/splitkb_aurora_lily58_right-nice_nano_v2-zmk.uf2"
+artifact_name=splitkb_aurora_lily58_right-nice_nano_v2-zmk
+export artifact_name=$artifact_name
+export display_name="splitkb_aurora_lily58_right nice_nano_v2"
+right=$(mktemp -d)
+west build -p always -s zmk/app -d "$right" -b "$board" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="splitkb_aurora_lily58_right" -DZMK_EXTRA_MODULES="$LILY58" -DCONFIG_ZMK_STUDIO=y || exit 1
+cp $right/zephyr/zmk.uf2 "/build/$artifact_name.uf2"
 
-# add reset firmware for good measure, can be disabled once you have it made
-export "display_name=${shield:+$shield_reset - }${board}"
-west build -p -s zmk/app -d "$BUILD_DIR" -b "nice_nano_v2" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="$shield_reset" -DZMK_EXTRA_MODULES="$BUILD_DIR" || exit 1
-cp "$BUILD_DIR/zephyr/zmk.uf2" "/build/nice_nano_v2_reset-zmk.uf2"
+artifact_name="settings_reset-nice_nano_v2-zmk"
+export artifact_name=$artifact_name
+export display_name="settings_reset nice_nano_v2"
+reset=$(mktemp -d)
+west build -p always -s zmk/app -d "$reset" -b "$board" -- -DZMK_CONFIG="$ZMK_CONFIG_DIR" -DSHIELD="settings_reset" -DZMK_EXTRA_MODULES="$LILY58" || exit 1
+cp $reset/zephyr/zmk.uf2 "/build/$artifact_name.uf2"
